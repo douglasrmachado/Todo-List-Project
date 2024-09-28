ecdf62dd0add8f880133e549b2c1841e2b73c441
douglasrmachado <douglasroqmachado@gmail.com>
2024-09-28 16:24:19 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+const button = document.querySelector('.button-add-task')
+const input = document.querySelector('.input-task')
+const listaCompleta = document.querySelector('.list-tasks')
+
+let minhaListaDeItens = []
+
+function adicionarNovaTarefa() {
+    minhaListaDeItens.push(input.value)
+
+    input.value = ''
+    mostrarTarefas();
+}
+
+function mostrarTarefas() {
+
+    let novaLi = ''
+
+   // ['comprar café', 'estudar programação']
+
+    minhaListaDeItens.forEach((tarefa) => {
+        novaLi = novaLi + `
+        <li class="task">
+          <img src="./img/checked.png" alt="check-na-tarefa">
+          <p>${tarefa}</p>
+          <img src="./img/trash.png" alt="tarefa-para-lixo">
+        </li> 
+        
+        `
+    })
+
+    listaCompleta.innerHTML = novaLi
+}
+
+button.addEventListener('click', adicionarNovaTarefa)
+
